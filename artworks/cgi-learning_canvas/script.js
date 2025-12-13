9256dd095b7f821de10fdde6cb280478ef369e8b
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-12-13 19:24:57 +0300

    feat(canvas): smile

--- artworks/cgi-learning_canvas/script.js
+++ artworks/cgi-learning_canvas/script.js
@@ -28,8 +28,21 @@ function drawLesson3(ctx) {
   ctx.fill();
 }
 
+function drawLesson4(ctx) {
+  ctx.beginPath();
+  ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Внешняя окружность
+  ctx.moveTo(110, 75);
+  ctx.arc(75, 75, 35, 0, Math.PI, false); // рот (по часовой стрелке)
+  ctx.moveTo(65, 65);
+  ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Левый глаз
+  ctx.moveTo(95, 65);
+  ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Правый глаз
+  ctx.stroke();
+}
+
 function draw() {
   drawLessonFactory(1, drawLesson1);
   drawLessonFactory(2, drawLesson2);
   drawLessonFactory(3, drawLesson3);
+  drawLessonFactory(4, drawLesson4);
 }
