6ef2ea71d36d65b23f2d7f0c408e796fe6a8c758
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-12-23 22:23:03 +0300

    feat(canvas): red channel crop & psd template

--- learning/canvas/channels/static/scripts/channels.js
+++ learning/canvas/channels/static/scripts/channels.js
@@ -16,20 +16,26 @@ img.onload = () => {
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   const data = imageData.data;
 
-  // setRedImageData(data);
+  setRedImageData(data);
   // setGreenImageData(data);
-  setBlueImageData(data);
+  // setBlueImageData(data);
 
   ctx.putImageData(imageData, 0, 0);
 };
 
 const setRedImageData = (data) => {
   for (var i = 0; i < data.length; i += 4) {
-    var avg = (data[i + 1] + data[i + 2]) / 2;
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    // const avg = (data[i + 1] + data[i + 2]) / 2;
 
+    // data[i] = 255;
+    // data[i + 1] = 255 - avg;
+    // data[i + 2] = 255 - avg;
+
+    data[i + 3] = data[i];
     data[i] = 255;
-    data[i + 1] = avg;
-    data[i + 2] = avg;
+    data[i + 1] = 0;
+    data[i + 2] = 0;
   }
 };
 
@@ -37,9 +43,9 @@ const setGreenImageData = (data) => {
   for (var i = 0; i < data.length; i += 4) {
     var avg = (data[i + 1] + data[i + 2]) / 2;
 
-    data[i] = avg;
+    data[i] = 255 - avg;
     data[i + 1] = 255;
-    data[i + 2] = avg;
+    data[i + 2] = 255 - avg;
   }
 };
 
