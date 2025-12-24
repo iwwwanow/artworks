1dd932f773e36892b1eea39419e7d1ff0ff45f69
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-12-24 22:57:32 +0300

    refactor(canvas): composition WIP

--- learning/canvas/channels/static/scripts/channels.js
+++ learning/canvas/channels/static/scripts/channels.js
@@ -1,75 +1,7 @@
 const CANVAS_ID = "channels-canvas";
-const IMAGE_WIDTH = 300;
-const IMAGE_HEIGHT = 400;
-
-const canvas = document.getElementById(CANVAS_ID);
-let ctx;
-
-if (canvas.getContext) {
-  ctx = canvas.getContext("2d");
-}
-
-const img = new Image();
-img.src = "assets/flowers.jpg";
-img.onload = () => {
-  ctx.drawImage(img, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
-  img.style.display = "none";
-
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const redLayerArrayData = getRedLayerData(imageData.data);
-  const redLayerImageData = new ImageData(
-    redLayerArrayData,
-    IMAGE_WIDTH,
-    IMAGE_HEIGHT,
-  );
-
-  // setRedImageData(data);
-  // setGreenImageData(data);
-  // setBlueImageData(data);
-
-  ctx.putImageData(redLayerImageData, 0, 0);
-};
-
-const setRedImageData = (data) => {};
-
-const getRedLayerData = (data) => {
-  let output = data;
-  for (var i = 0; i < data.length; i += 4) {
-    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-    // const avg = (data[i + 1] + data[i + 2]) / 2;
-
-    // data[i] = 255;
-    // data[i + 1] = 255 - avg;
-    // data[i + 2] = 255 - avg;
-
-    output[i + 3] = data[i];
-    output[i] = 255;
-    output[i + 1] = 0;
-    output[i + 2] = 0;
-  }
-  return output;
-};
-
-const setGreenImageData = (data) => {
-  for (var i = 0; i < data.length; i += 4) {
-    var avg = (data[i + 1] + data[i + 2]) / 2;
-
-    data[i] = 255 - avg;
-    data[i + 1] = 255;
-    data[i + 2] = 255 - avg;
-  }
-};
-
-const setBlueImageData = (data) => {
-  for (var i = 0; i < data.length; i += 4) {
-    var avg = (data[i + 1] + data[i + 2]) / 2;
-
-    data[i] = avg;
-    data[i + 1] = avg;
-    data[i + 2] = 255;
-  }
-};
+const IMAGE_SRC = "assets/flowers.jpg";
 
 const drawChannelsProject = (img) => {
-  ctx.drawImage(img, 0, 0);
+  const composition = new Composition(CANVAS_ID, IMAGE_SRC);
+  composition.init();
 };
---
+++ learning/canvas/channels/static/scripts/composition.class.js
@@ -0,0 +1,71 @@
+const IMAGE_WIDTH = 300;
+const IMAGE_HEIGHT = 400;
+
+class Composition {
+  constructor(canvasId, imageSrc) {
+    this.canvasId = canvasId;
+    this.imageSrc = imageSrc;
+  }
+
+  init() {
+    this.#initCanvas();
+    this.#initImage();
+  }
+
+  #initCanvas() {
+    this.canvas = document.getElementById(this.canvasId);
+    if (this.canvas.getContext) {
+      this.ctx = this.canvas.getContext("2d");
+    }
+  }
+
+  #initImage() {
+    this.img = new Image();
+    this.img.src = this.imageSrc;
+    this.img.style.display = "none";
+    this.img.onload = this.#onImageLoadHander();
+  }
+
+  #onImageLoadHander() {
+    this.ctx.drawImage(this.img, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    const imageData = this.ctx.getImageData(
+      0,
+      0,
+      this.canvas.width,
+      this.canvas.height,
+    );
+    // const redLayerArrayData = getRedLayerData(imageData.data);
+    const redLayerArrayData = imageData.data.map((item) => 128);
+    console.log(redLayerArrayData);
+    const redLayerImageData = new ImageData(
+      redLayerArrayData,
+      IMAGE_WIDTH,
+      IMAGE_HEIGHT,
+    );
+
+    // setRedImageData(data);
+    // setGreenImageData(data);
+    // setBlueImageData(data);
+
+    this.ctx.putImageData(redLayerImageData, 0, 0);
+  }
+}
+
+const getRedLayerData = (data) => {
+  let output = data;
+  for (var i = 0; i < data.length; i += 4) {
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    // const avg = (data[i + 1] + data[i + 2]) / 2;
+
+    // data[i] = 255;
+    // data[i + 1] = 255 - avg;
+    // data[i + 2] = 255 - avg;
+
+    output[i + 3] = data[i];
+    output[i] = 255;
+    output[i + 1] = 0;
+    output[i + 2] = 0;
+  }
+  return output;
+};
